46349160d6caaf5cf61edcb373ba616bec55abae
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-05-13 18:06:46 +0900

    test

--- project/config/urls.py
+++ project/config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ltv import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("ltv/", views.index),
 ]
