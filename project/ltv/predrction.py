4c5033b4b93fb27d684e620fed347373e5763e5a
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2021-06-04 01:13:52 +0900

    프론트 적용, 예측 모델 최신화

--- project/ltv/predrction.py
+++ project/ltv/predrction.py
@@ -18,11 +18,7 @@ def predict(start, end, percentile):
     print(f"현재 {device} 사용중")
 
     load = torch.load(
-        os.path.join(STATICFILES_DIRS[0], "model/model3.pth"), map_location="cpu"
-    )  # 그저 test 데이터 입력값 불러오려고만 쓰는거임
-
-    load2 = torch.load(
-        os.path.join(STATICFILES_DIRS[0], "model/model4.pth"), map_location="cpu"
+        os.path.join(STATICFILES_DIRS[0], "model/model2.pth"), map_location="cpu"
     )  # 그저 test 데이터 입력값 불러오려고만 쓰는거임
 
     # return HttpResponse(json.dumps(load), status=200)
@@ -35,61 +31,31 @@ def predict(start, end, percentile):
 
     # 모델 바로 불러오기
     model_pt = torch.load(
-        os.path.join(STATICFILES_DIRS[0], "model/model3.pt"), map_location="cpu"
+        os.path.join(STATICFILES_DIRS[0], "model/model2.pt"), map_location="cpu"
     )  # model 전체 불러오기
-    # print(load["test_features"][0].shape)
-    # print(load2["test_features"].shape)
-    # print(type(load["test_features"][0]))
-    # print(type(load2["test_features"]))
-    # print(load["test_features"][0])
-    # print(load2["test_features"])
-    x_test = np.array(load["test_features"][0])  # 입력값
-    x_test2 = np.array(load2["test_features"])  # 입력값
-    print(load["test_features"][0].info())
-    print("-------------------------------")
-    print(load2["test_features"].info())
 
-    # print(x_test.dtypes)
-    # print(type(load["test_target"]))
-    # print(type(load2["test_target"]))
-    # print(load["test_target"].dtypes)
-    # print(load2["test_target"].dtypes)
-    print(x_test)
-    print(x_test2)
+    x_test = np.array(load["test_features"][0])  # 입력값
     y_test = np.array(load["test_target"])  # 출력값
+    print(min(load["test_features"][1]))
+    print(max(load["test_features"][1]))
     x_test = torch.tensor(x_test).to(device)  # 텐서화
     y_test = torch.tensor(y_test).to(device)  # 텐서화
 
-    # rand_idx = torch.randperm(x_test.size(0)).to(device)
-    # x_test = torch.index_select(x_test, dim=0, index=rand_idx)
-    # y_test = torch.index_select(y_test, dim=0, index=rand_idx)
-
-    # print("x_test : ", x_test)
-    # print(x_test.shape)
     model_pt.eval()
 
     predict_y = model_pt(x_test.float())
-    predict_y = predict_y.sum(dim=1)
-    # print(predict_y.shape)
-    # print("prediction : ", predict_y)
+    ads = predict_y.sum(dim=0)
+    # predict_y = predict_y.sum(dim=1)  # output 3개 하나로 합치기(rv is ba)
 
     # 테스트용
-    start = datetime(start[0:5], start[6:8], start[9:11])  # "2021-05-24"
-    end = datetime(end[0:5], end[6:8], end[9:11])  # "2021-06-06"
+    start = datetime(int(start[0:4]), int(start[5:7]), int(start[8:10]))  # "2021-05-24"
+    end = datetime(int(end[0:4]), int(end[5:7]), int(end[8:10]))  # "2021-06-06"
 
     # date, per 작업
-    result = predict_y.flatten().tolist()
+    result = predict_y.tolist()
+    # result = predict_y.flatten().tolist()
 
-    # 테스트용
-    d = start
-    result_date = []
-    for i in range(0, 10):
-        result_date.extend([d] * (len(result) // 10))
-        d += timedelta(days=7)
-    # result_date = [datetime(2021, 5, 24)] * (len(result) // 2)
-    # result_date.extend([datetime(2021, 5, 31)] * (len(result) - len(result) // 2))
-
-    # result_date = [0] * len(result)  # test데이터의 날짜 목록
+    result_date = load["test_features"][1]
 
     result = list(zip(result, result_date))
     result.sort(key=lambda x: x[0])
@@ -113,14 +79,27 @@ def devide_period(data, start, end):
 
     total = 0
     user = 0
+    ads_ratio = {"is": 0, "rv": 0, "ba": 0}
+
     for item in data:
         for n, s, e in week_criteria:
             if s <= item[1] <= e:
-                result["data"][n] += item[0]
-                total += item[0]
+                result["data"][n] += sum(item[0])
+                ads_ratio["is"] += item[0][0]
+                ads_ratio["rv"] += item[0][1]
+                ads_ratio["ba"] += item[0][2]
+                total += sum(item[0])
                 user += 1
                 break
+    result["data"] = {k: v for k, v in sorted(result["data"].items(), key=lambda x: -x[1])}
+    result["max"] = 0
+    result["min"] = float("inf")
+    for k, v in result["data"].items():
+        result["max"] = max(result["max"], v)
+        result["min"] = min(result["min"], v)
     result["total"] = total
     result["user"] = user
     result["avg"] = total / user
+    result["ads_ratio"] = ads_ratio
+
     return result
--- project/ltv/views.py
+++ project/ltv/views.py
@@ -13,9 +13,7 @@ def index(request):
 
 
 def income_predict(request):
-    predict_list = predict(
-        request.GET["from"], request.GET["to"], request.GET["percentile"]
-    )
+    predict_list = predict(request.GET["from"], request.GET["to"], request.GET["percentile"])
 
     return HttpResponse(json.dumps(predict_list), status=200)
 
@@ -32,9 +30,7 @@ def test_json(request):
         temp = MongoDbManager().get_users_from_collection(
             {"ad_id": "cfc31233-e5f7-4eb2-a442-0b2df5f2f83f"}
         )
-        return HttpResponse(
-            json.dumps({"id": temp[0]["ad_id"]}, default=str), status=200
-        )
+        return HttpResponse(json.dumps({"id": temp[0]["ad_id"]}, default=str), status=200)
 
     else:
         return HttpResponse(status=405)
@@ -42,9 +38,7 @@ def test_json(request):
 
 def device_os_analysis(request):
     if request.method == "GET":
-        percentile = int(
-            MongoDbManager().database_len * float(request.GET["percentile"])
-        )
+        percentile = int(MongoDbManager().database_len * float(request.GET["percentile"]))
         pipeline = make_pipeline("device_operating_system_version", percentile)
         result = list(MongoDbManager().database.aggregate(pipeline))
         for item in result:
@@ -56,9 +50,7 @@ def device_os_analysis(request):
 
 def weekday_analysis(request):
     if request.method == "GET":
-        percentile = int(
-            MongoDbManager().database_len * float(request.GET["percentile"])
-        )
+        percentile = int(MongoDbManager().database_len * float(request.GET["percentile"]))
         week = [
             "Monday",
             "Tuesday",
@@ -80,14 +72,16 @@ def weekday_analysis(request):
 
 def device_name_analysis(request):
     if request.method == "GET":
-        percentile = int(
-            MongoDbManager().database_len * float(request.GET["percentile"])
-        )
+        percentile = int(MongoDbManager().database_len * float(request.GET["percentile"]))
         pipeline = make_pipeline("device_mobile_marketing_name", percentile)
         result = list(MongoDbManager().database.aggregate(pipeline))
+        print(result)
         for item in result:
             if item["_id"] == None:
                 item["device_name"] = "Uncertain"
+                # None 제거 코드
+                item["count"] = 0
+                item["percentile"] = 0
             else:
                 item["device_name"] = item["_id"]
             del item["_id"]
@@ -96,9 +90,7 @@ def device_name_analysis(request):
 
 def region_analysis(request):
     if request.method == "GET":
-        percentile = int(
-            MongoDbManager().database_len * float(request.GET["percentile"])
-        )
+        percentile = int(MongoDbManager().database_len * float(request.GET["percentile"]))
         pipeline = make_pipeline("geo_region", percentile)
         result = list(MongoDbManager().database.aggregate(pipeline))
         for item in result:
@@ -112,13 +104,11 @@ def region_analysis(request):
 
 def time_analysis(request):
     if request.method == "GET":
-        percentile = int(
-            MongoDbManager().database_len * float(request.GET["percentile"])
-        )
+        percentile = int(MongoDbManager().database_len * float(request.GET["percentile"]))
         pipeline = make_pipeline("hour", percentile)
         result = list(MongoDbManager().database.aggregate(pipeline))
         for item in result:
-            item["hour"] = item["_id"]
+            item["hour"] = (int(item["_id"]) + 13) % 24
             del item["_id"]
         result.sort(key=lambda x: x["hour"])
 
